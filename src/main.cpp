8517758f75ea265af9085806aede00bc6b359e24
Bardh <bardh.tafarshiku@gmail.com>
2025-02-10 17:50:35 -0500

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,17 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
+  while(true) {
+    // Uncomment this block to pass the first stage
+    std::cout << "$ ";
+    
+    // std::string input;
+    std::string input;
+    std::getline(std::cin, input);
+    
+    std::cout << input << ": command not found" << std::endl;
+    std::getline(std::cin, input);
+  }
 
-  // std::string input;
-  std::string input;
-  std::getline(std::cin, input);
-
-  std::cout << input << ": command not found" << std::endl;
-
-  std::getline(std::cin, input);
+  return 0;
 }
