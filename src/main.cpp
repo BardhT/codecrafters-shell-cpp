b8756af86829774c9cb47e09df77ede67aaf134f
Bardh <bardh.tafarshiku@gmail.com>
2025-02-10 18:01:48 -0500

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ostream>
+#include <sstream>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,11 +14,17 @@ int main() {
     // std::string input;
     std::string input;
     std::getline(std::cin, input);
+    std::stringstream ss(input);
+    std::string command;
+    ss >> command;
 
-    if(input == "exit 0") {
+    if(command == "exit 0") {
       break;
-    }
-    
+    } else if (command == "echo") {
+      std::string output = ss.str();
+      std::cout << output << std::endl;
+      continue;
+    }    
     std::cout << input << ": command not found" << std::endl;
   }
 
