1fc000702544ded042968462710fce3f70600c45
Bardh <bardh.tafarshiku@gmail.com>
2025-02-10 18:29:18 -0500

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <ostream>
 #include <sstream>
+#include <unordered_set>
+
+std::unordered_set<std::string> builtIn = {"echo", "exit", "type"};
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -17,14 +20,24 @@ int main() {
     std::stringstream ss(input);
     std::string command;
     ss >> command;
-
+    
     if(command == "exit") {
       break;
     } else if (command == "echo") {
       std::string output = ss.str();
       std::cout << output.substr(5) << std::endl;
       continue;
-    }    
+    } else if (command == "type") {
+      std::string subcommand;
+      ss >> subcommand;
+      if (builtIn.find(subcommand) != builtIn.end()) {
+        std::cout << subcommand << " is a shell builtin" << std::endl;
+      } else{
+        std::cout << subcommand << ": command not found" << std::endl;
+        continue;
+      }
+    }
+
     std::cout << input << ": command not found" << std::endl;
   }
 
