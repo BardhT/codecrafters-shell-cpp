cf8da50fca57b3d079a21677af15fbfbb2d27613
Bardh <bardh.tafarshiku@gmail.com>
2025-02-10 22:31:35 -0500

    External commands finished!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
@@ -5,6 +6,7 @@
 #include <unordered_set>
 #include <vector>
 #include <unistd.h>
+#include <sys/wait.h>
 
 std::unordered_set<std::string> builtIn = {"echo", "exit", "type"};
 
@@ -33,36 +35,75 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
     std::stringstream ss(input);
-    std::string command;
-    ss >> command;
+
+    // Tokenize and validate input
+    std::vector<std::string> tokens;
+    std::string token;
+    while (ss >> token) {
+      tokens.push_back(token);
+    }
+    if (tokens.empty()) continue;
     
-    if(command == "exit") {
+    if(tokens[0] == "exit") {
       break;
-    } else if (command == "echo") {
-      std::string output = ss.str();
-      std::cout << output.substr(5) << std::endl;
+    } else if (tokens[0] == "echo") {
+      std::string output;
+      
+      if (tokens.size() > 1) output += tokens[1];
+      else {
+        std::cout << std::endl;
+        continue;
+      }
+
+      for (size_t i = 2; i < tokens.size(); i++){
+        output += " " + tokens[i];
+      }
+
+      std::cout << output << std::endl;
       continue;
-    } else if (command == "type") {
-      std::string subcommand;
-      ss >> subcommand;
-      if (builtIn.find(subcommand) != builtIn.end()) {
-        std::cout << subcommand << " is a shell builtin" << std::endl;
+    } else if (tokens[0] == "type") {
+      if (tokens.size() <= 1) continue;
+      if (builtIn.find(tokens[1]) != builtIn.end()) {
+        std::cout << tokens[1] << " is a shell builtin" << std::endl;
         continue;
       } else {
         // Check the PATH directories
         bool path = false;
         for (std::string s : envs) {
-          std::string full = s + "/" + subcommand;
+          std::string full = s + "/" + tokens[1];
           if (access(full.c_str(), X_OK) == 0) {
-            std::cout << subcommand << " is " << full << std::endl;
+            std::cout << tokens[1] << " is " << full << std::endl;
             path = true;
             break;
           }
         }
         if(!path) {
-          std::cout << subcommand << ": not found" << std::endl;
+          std::cout << tokens[1] << ": not found" << std::endl;
         }
         continue;
+      }
+    } else {
+      pid_t pid = fork();
+
+      if (pid > 0) {  // Parent
+        int status;
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status)){
+          if (WEXITSTATUS(status) == 127) {
+            std::cout << input << ": command not found" << std::endl;
+          }
+          continue;
+        }
+      } else if (pid == 0) {  // Child
+        char* args[tokens.size() + 1];
+
+        for(size_t i = 0; i < tokens.size(); i++) {
+          args[i] = (char*)tokens[i].c_str();
+        }
+        args[tokens.size()] = nullptr;
+        execvp(tokens[0].c_str(), args);
+        exit(127);
       }
     }
 
